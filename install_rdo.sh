02afcc7f6cdeba7d1a3c8fefeaeec1208805d6ee
Suraj Deshmukh <surajssd009005@gmail.com>
2015-11-27 22:11:56 +0530

    Added shell file

---
+++ install_rdo.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+echo '[*] Checking for Updates'
+yum -y update
+if [ $? -ne 0 ]; then
+    echo "[!] Unable to update, check your internet connection!"
+    exit 0
+fi
+
+echo '[*] Disabling Network Manager'
+systemctl stop NetworkManager
+systemctl disable NetworkManager
+systemctl enable netwsuork
+
+echo '[*] Setting up RDO repositories'
+yum install -y https://rdoproject.org/repos/rdo-release.rpm
+if [ $? -ne 0 ]; then
+    echo "[!] Unable to setup RDO repos, check your internet connection!"
+    exit 0
+fi
+
+echo '[*] Installing Packstack Installer'
+yum install -y openstack-packstack
+if [ $? -ne 0 ]; then
+    echo "[!] Unable to install packstack installer, check your internet connection!"
+    exit 0
+fi
+
+echo '[*] Starting packstack'
+packstack --allinone
+if [ $? -ne 0 ]; then
+    echo "[!] Something went wrong!!"
+    exit 0
+fi
